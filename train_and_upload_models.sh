828504d6b9cda9d90ee3baa193d1e222019834ee
Steve Mickeler <smickeler@C02Y46CRJG5J-MBP.globeandmail.net>
2019-04-25 16:06:41 -0400

    update upload bucket

--- train_and_upload_models.sh
+++ train_and_upload_models.sh
@@ -1,5 +1,5 @@
 for langCode in "en-US" "lv-LV" "es-MX" "de-AT" "en-CA" "nl-BE" "da-DK" "en-AU" "en-IE" "it-IT" "de-DE" "fr-CA" "sv-SE" "fi-FI" "de-CH" "nl-NL" "ka-GE"; do
 	/usr/local/bin/pipenv run ./classify build --lang $langCode
-	aws s3 cp --acl public-read data/$langCode/classifier.dill s3://pp-data/fbpac-models/$langCode/classifier.dill
+	aws s3 cp data/$langCode/classifier.dill s3://tgam-fbpac-models/$langCode/classifier.dill
 done
 

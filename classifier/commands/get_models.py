26abd693b0450a4e144a044b766b9711e6761b85
Steve Mickeler <smickeler@C02Y46CRJG5J-MBP.globeandmail.net>
2019-04-25 22:28:02 -0400

    add classifier_file var

--- classifier/commands/get_models.py
+++ classifier/commands/get_models.py
@@ -20,12 +20,7 @@ def get_models(ctx, lang):
         if lang and conf["language"] != lang:
             continue
         model_path = "data/{}/classifier.dill".format(conf["language"])
+        classifier_file = "{}/classifier.dill".format(conf["language"])
         # call(["wget", "-nv", "-O", model_path, "https://s3.amazonaws.com/pp-data/fbpac-models/{}/classifier.dill".format(conf["language"])])
-        s3.download_file(
-            "tgam-fbpac-models",
-            "{}/classifier.dill".format(conf["language"]),
-            "data/{}/classifier.dill".format(conf["language"]),
-        )
-        print(str("{}/classifier.dill".format(conf["language"])))
-        print(str("data/{}/classifier.dill".format(conf["language"])))
-
+        print("Fetching " + classifier_file + " from S3")
+        s3.download_file("tgam-fbpac-models", classifier_file, model_path)
